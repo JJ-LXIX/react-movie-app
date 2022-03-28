f2a114e4e918d24fdc0244f39f22f7e2a9d937a3
Jagrath <jagrathshettylxix@gmail.com>
2022-03-28 16:17:51 +0530

    initial commit (Header and Routes added)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.1.2/css/boxicons.min.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Routes1 from "./config/Routes1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/*"
+          element={
+            <>
+              <Header />
+              <Routes1 />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
