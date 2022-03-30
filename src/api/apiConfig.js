89761ca2ed75b59748089041563a697b5a2f48e3
Jagrath <jagrathshettylxix@gmail.com>
2022-03-30 14:02:10 +0530

    button and modal added , hero-lside half done

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: `91665036ffa5773bb8fe907fe0a3511f`,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
